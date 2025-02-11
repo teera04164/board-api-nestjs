ad06064093f8da92a8575826320eb363e0644936
Teerapong <teera04164@gmail.com>
2025-02-11 19:27:13 +0700

    add comment entity

---
+++ src/modules/comments/entities/comment.entity.ts
@@ -0,0 +1,39 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+} from 'typeorm';
+import { User } from '../../users/entities/user.entity';
+import { Post } from '../../posts/entities/post.entity';
+
+@Entity('comments')
+export class Comment {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('text')
+  content: string;
+
+  @ManyToOne(() => User)
+  user: User;
+
+  @Column()
+  userId: string;
+
+  @ManyToOne(() => Post, (post) => post.comments, {
+    onDelete: 'CASCADE',
+  })
+  post: Post;
+
+  @Column()
+  postId: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
